b62499f4b9dd9c65e2f8480b9917b7300972d6f1
Bjorn Persson Mattsson <bjorn.pm@plankter.se>
2015-03-29 19:45:40 +0200

    Having problems getting a collection of states to work

--- TEST/Astar.ts
+++ TEST/Astar.ts
@@ -1,12 +1,14 @@
 class State {
 	id : number;
 	edges : collections.Set<Edge>;
+	cost : number;
 	heuristic : number;
 	target : boolean;
 
 	constructor(id : number) {
 		this.id = id;
 		this.edges = new collections.Set<Edge>();
+		this.cost = 0;
 		this.heuristic = 0;
 		this.target = false;
 	}
@@ -35,20 +37,24 @@ class State {
 		return this.target;
 	}
 
+	getID() {
+		return this.id;
+	}
+
 	toString() {
-		return "State";
+		return collections.makeString(this);
 	}
 }
 
 class Edge {
 	cost : number;
-	fromID : number;
-	toID : number;
+	from : State;
+	to : State;
 
-	constructor(cost, fromID, toID) {
+	constructor(cost, from, to) {
 		this.cost = cost;
-		this.fromID = fromID;
-		this.toID = toID;
+		this.from = from;
+		this.to = to;
 	}
 
 	getCost() {
@@ -56,12 +62,37 @@ class Edge {
 	}
 
 	toString() {
-		return "Edge from " + this.fromID + " to " + this.toID + ". Cost: " + this.cost;
+		return "Edge from " + this.from.toString() + " to " + this.to.toString() + ". Cost: " + this.cost;
 	}
 }
+/*
+function performSetup() {
+	var tmp1 = new State(1);
+	var tmp2 = new State(2);
+	var tmp3 = new State(3);
+	var tmp4 = new State(4);
+	var tmp5 = new State(5);
 
-var test = new Edge(9, 1, 2);
+	createConnection(tmp1, tmp3, 5);
+
+	states.add(tmp1);
+	states.add(tmp2);
+	states.add(tmp3);
+	states.add(tmp4);
+	states.add(tmp5);
+}
+
+function createConnection(from : State, to : State, cost : number) {
+	var e = new Edge(cost, from, to);
+	from.addEdge(e);
+}
+*/
+
+//var test = new Edge(9, 1, 2);
+var states = new collections.Set<State>();
+var outputString = "Output not overwritten";
 
 function main() {
-	document.getElementById("demo").innerHTML = test.toString();
+	//performSetup();
+	document.getElementById("demo").innerHTML = outputString;
 }
