464197268a9a7478c9c6f83e7cab1c9efaa944a9
Jeff Chen <jeff.polk230@gmail.com>
2015-03-31 22:02:31 +0200

    BFS & DFS implemented, need Nod implementation still in order to have some bookkeeping for back tracing the path

--- AStar/Searching.ts
+++ AStar/Searching.ts
@@ -10,7 +10,7 @@ Collection of searching :
    instead of keeping all nodes, what we need is just neighbouring functions 
 */
 
-interface Neighbours<T> {
+interface Neighbours<T> { // otherwise called sucesscor
     (me : T) : T[];
 }
 
@@ -22,6 +22,59 @@ interface GoalTest<T> {
     (amIgoal :T) : boolean; 
 }
 // These three should be enough to "Black Boxed" States
+
+/*SEARCHES*/
+
+function breadthFirst<T>(g :Graph<T>, start :T, goalTest :GoalTest<T>){
+    var q = new collections.Queue<T>();
+    var explored = [];
+    q.enqueue(start);
+    explored.push(start);
+    while(! q.isEmpty()){
+	var v = q.dequeue();
+	if (goalTest(v)){ return explored; } // reconstruction to real path?
+	else {
+	    var neighs = getNeighbours(g, v);
+	    for (var i = 0; i < neighs.length; i++){
+		if(!containing(explored, neighs[i])){
+		    //neighs[i].parent = v; // need a Nod still
+		    q.enqueue(neighs[i]);
+		    explored.push(neighs[i]);
+		}
+	    }
+	}
+    }
+    return undefined;
+}
+
+function depthFirst<T>(g :Graph<T>, start :T, goalTest :GoalTest<T>){
+    var q = new collections.Stack<T>();
+    var explored = [];
+    q.push(start);
+    explored.push(start);
+    while(! q.isEmpty()){
+	var v = q.pop();
+	if (goalTest(v)){ return explored; } 
+	else {
+	    var neighs = getNeighbours(g, v);
+	    for (var i = 0; i < neighs.length; i++){
+		if(!containing(explored, neighs[i])){
+		    q.push(neighs[i]);
+		    explored.push(neighs[i]);
+		}
+	    }
+	}
+    }
+    return undefined;
+}
+
+// helper funciton
+function containing<T>(list :T[], elem :T) :boolean{
+    for (var i = 0; i < list.length; i++){
+	if (elem == list[i]){return true;}
+    }
+    return false;
+}
 
 /*GRAPH STUFF*/
 class Graph<T>{
@@ -37,7 +90,7 @@ function addNode<T>(g :Graph<T>, node :T){
 
 function addEdge<T>(g :Graph<T>, start :T, end :T, cost :number){
     g.graph.getValue(start).setValue(end, cost);
-    g.graph.getValue(end).setValue(start, cost);
+    //g.graph.getValue(end).setValue(start, cost);
 }
 
 function getNeighbours<T>(g :Graph<T>, node :T) :T[]{
@@ -58,8 +111,27 @@ var tileGrid = new Graph<Tile>();
 var tileA = new Tile(0,0);
 var tileB = new Tile(0,1);
 var tileC = new Tile(1,1);
+var tileD = new Tile(2,2);
 addNode(tileGrid, tileA);
 addNode(tileGrid, tileB);
+addNode(tileGrid, tileC);
+addNode(tileGrid, tileD);
 addEdge(tileGrid, tileA, tileB, 1);
-//console.log(getNeighbours(tileGrid, tileB));
+addEdge(tileGrid, tileA, tileC, 10);
+addEdge(tileGrid, tileB, tileC, 2);
+addEdge(tileGrid, tileC, tileD, 10);
+addEdge(tileGrid, tileD, tileC, 10);
 
+var neighboursOfB = getNeighbours(tileGrid, tileA); 
+for (var i = 0; i < neighboursOfB.length; i++){
+    console.log(neighboursOfB[i].toString()); 
+}
+
+function tileGoalTest (t :Tile) :boolean{
+    if (t == tileC){
+	return true;
+    }
+    return false;
+}
+
+console.log(breadthFirst(tileGrid, tileA, tileGoalTest));
